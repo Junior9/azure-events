3e3d1aa687db9321f03c4e53d30dc852e798e87d
Junior9 <moraes.junior6@gmail.com>
2024-12-27 14:35:11 -0600

    Azure evet webhook

---
+++ src/main/java/com/example/events/subscriptions/EventBlobs.java
@@ -0,0 +1,16 @@
+package com.example.events.subscriptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventBlobs {
+
+    @GetMapping("/event/blob")
+    public ResponseEntity<String> addBlobEvent(){
+
+        return ResponseEntity.ok("New Blob created");
+    }
+
+}
