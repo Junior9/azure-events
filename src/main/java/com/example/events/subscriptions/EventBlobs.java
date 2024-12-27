65f236d853979834e6da25f341aec1eb60a2730f
Junior9 <moraes.junior6@gmail.com>
2024-12-27 15:00:47 -0600

    add logs

--- src/main/java/com/example/events/subscriptions/EventBlobs.java
+++ src/main/java/com/example/events/subscriptions/EventBlobs.java
@@ -1,5 +1,7 @@
 package com.example.events.subscriptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventBlobs {
 
+    protected static Logger logger = LoggerFactory.getLogger(EventBlobs.class);
+
     @GetMapping("/event/blob")
     public ResponseEntity<String> addBlobEvent(){
+        logger.info("[BLOB CREATED] ");
         return ResponseEntity.ok("New Blob created");
     }
 
