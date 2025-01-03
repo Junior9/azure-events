af8014deabfb1449f5a30a2f902e22301a8d20e3
Junior9 <moraes.junior6@gmail.com>
2025-01-03 11:25:25 -0600

    new version

--- src/main/java/com/example/events/common/KeyVault.java
+++ src/main/java/com/example/events/common/KeyVault.java
@@ -16,7 +16,7 @@ public class KeyVault {
     protected static Logger logger = LoggerFactory.getLogger(KeyVault.class);
 
     SecretClient secretClient = new SecretClientBuilder()
-    .vaultUrl("https://azkeyvault204.vault.azure.net/")
+    .vaultUrl("https://keyvaultaz204service.vault.azure.net/")
     .credential(new DefaultAzureCredentialBuilder().build())
     .buildClient();
 
